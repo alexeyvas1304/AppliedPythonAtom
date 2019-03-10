55b58d32735c3b4ec0df7237cf7518efe1c80b43
alexey <nikson1708@yandex.ru>
2019-03-10 20:35:51 +0300

    simple vkposter done

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -9,7 +9,9 @@ from homeworks.homework_02.fastmerger import FastSortedListMerger
 class VKPoster:
 
     def __init__(self):
-        raise NotImplementedError
+        self.user_subsc = {}
+        self.user_posts = {}
+        self.read_posts = {}
 
     def user_posted_post(self, user_id: int, post_id: int):
         '''
@@ -19,7 +21,10 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
-        pass
+        if user_id not in self.user_posts:
+            self.user_posts[user_id] = [post_id]
+        else:
+            self.user_posts[user_id].append(post_id)
 
     def user_read_post(self, user_id: int, post_id: int):
         '''
@@ -29,7 +34,12 @@ class VKPoster:
         :param post_id: id поста. Число.
         :return: ничего
         '''
-        pass
+        if post_id not in self.read_posts:
+            self.read_posts[post_id] = [1, [user_id]]
+        elif post_id in self.read_posts and user_id \
+                not in self.read_posts[post_id][1]:
+            self.read_posts[post_id][0] += 1
+            self.read_posts[post_id][1].append(user_id)
 
     def user_follow_for(self, follower_user_id: int, followee_user_id: int):
         '''
@@ -39,9 +49,12 @@ class VKPoster:
         :param followee_user_id: id пользователя. Число.
         :return: ничего
         '''
-        pass
+        if follower_user_id not in self.user_subsc:
+            self.user_subsc[follower_user_id] = [followee_user_id]
+        else:
+            self.user_subsc[follower_user_id].append(followee_user_id)
 
-    def get_recent_posts(self, user_id: int, k: int)-> list:
+    def get_recent_posts(self, user_id: int, k: int) -> list:
         '''
         Метод который вызывается когда пользователь user_id
         запрашивает k свежих постов людей на которых он подписан.
@@ -50,14 +63,27 @@ class VKPoster:
         :return: Список из post_id размером К из свежих постов в
         ленте пользователя. list
         '''
-        pass
+        lst = []
+        if user_id not in self.user_subsc:
+            return lst
+        for subscriber in self.user_subsc[user_id]:
+            if subscriber in self.user_posts:
+                lst += self.user_posts[subscriber]
+                lst = sorted(lst, reverse=True)
+        return lst[0:k]
 
     def get_most_popular_posts(self, k: int) -> list:
         '''
-        Метод который возвращает список k самых популярных постов за все время,
-        остортированных по свежести.
+        Метод который возвращает список k самых популярных постов за
+        все время,остортированных по свежести.
         :param k: Сколько самых свежих популярных постов
         необходимо вывести. Число.
         :return: Список из post_id размером К из популярных постов. list
         '''
-        pass
+        lst = []
+        sorted_by_count = sorted(self.read_posts.items(),
+                                 key=lambda kv: (kv[1][0], kv[0]))
+        sorted_by_count.reverse()
+        for i in range(k):
+            lst.append(sorted_by_count[i][0])
+        return lst
