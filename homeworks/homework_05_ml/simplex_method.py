cc4a4e31b47ad642a4e992abaddb9a1637616f48
Alex <nikson1708@yandex.ru>
2019-04-07 18:40:41 +0300

    obligatory

--- homeworks/homework_05_ml/simplex_method.py
+++ homeworks/homework_05_ml/simplex_method.py
@@ -20,4 +20,32 @@ def simplex_method(a, b, c):
     :param c: np.array, shape=(1, m)
     :return x: np.array, shape=(1, m)
     """
-    raise NotImplementedError
+
+    n, m = a.shape
+    c *= -1
+    matrix = np.hstack([np.vstack([a, c]),
+                        np.eye(n + 1),
+                        np.vstack([b.reshape(n, 1), np.array([0])])])
+
+    def subtr(a, b, f):  # пеп ругался на лямбду, поэтому сделал отдельную функцию
+        return a - b * a[f] / b[f]
+
+    while True:
+        if all(matrix[-1] >= 0):
+            break
+        column_index = matrix[-1].argmin()
+        seq = matrix[:-1, -1] / matrix[:-1, column_index]
+        row_index = seq.argmin()
+
+        for i in range(n + 1):
+            if i != row_index:
+                matrix[i] = subtr(matrix[i], matrix[row_index], column_index)
+            else:
+                matrix[i] /= matrix[row_index][column_index]
+
+        x = np.zeros(m)
+        for i in range(m):
+            if matrix[-1][i] == 0:
+                index_of_1 = matrix[:, i].argmax()
+                x[i] = matrix[index_of_1][-1]
+    return x
