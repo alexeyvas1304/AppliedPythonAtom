2b0dbdd455c5f4289096a354163911570e89591f
Alex <nikson1708@yandex.ru>
2019-04-23 02:04:03 +0300

    not all

--- homeworks/homework_07_ml/logreg/logistic_regression.py
+++ homeworks/homework_07_ml/logreg/logistic_regression.py
@@ -6,14 +6,24 @@ import numpy as np
 
 
 class LogisticRegression:
-    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5):
+    def __init__(self, lambda_coef=0.1, regularization=None, alpha=0.3, eps=0.0001, iterations=5000, flag=False):
         """
         LogReg for Binary case
         :param lambda_coef: constant coef for gradient descent step
         :param regulatization: regularizarion type ("L1" or "L2") or None
         :param alpha: regularizarion coefficent
         """
-        pass
+        self.lambda_coef = lambda_coef
+        self.regularization = regularization
+        self.eps = eps
+        self.alpha = alpha
+        self.iterations = iterations
+        self.flag = flag
+
+    def check(self, X, y):
+        if X.shape[0] != y.shape[0]:
+            raise ValueError
+        return True
 
     def fit(self, X_train, y_train):
         """
@@ -22,7 +32,32 @@ class LogisticRegression:
         :param y_train: target values for training data
         :return: None
         """
-        pass
+        if self.check(X_train, y_train):
+            n = X_train.shape[0]
+            k = X_train.shape[1]
+            self.mean_x = np.mean(X_train, axis=0)
+            self.std_x = np.std(X_train, axis=0)
+            X_train = (X_train - self.mean_x) / self.std_x
+            X_train = np.hstack([np.ones((n, 1)), X_train])
+            self.w = np.random.randn(k + 1, 1)
+            cnt = 0
+            while True:
+                sigm_dist = 1 / (1 + np.exp(-X_train @ self.w))
+                deriv_const = 1 / n * (X_train.T @ (sigm_dist - y_train.reshape(n, 1))).T
+                if self.regularization == 'L1':
+                    add = self.alpha * np.sign(self.w).reshape(1, -1)
+                elif self.regularization == 'L2':
+                    add = 2 * self.alpha * self.w.T
+                else:
+                    add = 0
+                full_deriv = deriv_const + add
+                tmp = self.w
+                self.w = self.w - self.lambda_coef * full_deriv.T
+                cnt += 1
+                if np.sum((tmp - self.w) ** 2) < self.eps or cnt == self.iterations:
+                    break
+            self.flag = True
+            return None
 
     def predict(self, X_test):
         """
@@ -30,7 +65,12 @@ class LogisticRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        pass
+        if not self.flag:
+            raise Exception()
+        probabilities = self.predict_proba(X_test).reshape(-1)
+        y_test = np.zeros(probabilities.shape[0])
+        y_test[probabilities >= 0.5] = 1
+        return y_test
 
     def predict_proba(self, X_test):
         """
@@ -38,11 +78,20 @@ class LogisticRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted probabilities
         """
-        pass
+        if not self.flag:
+            raise Exception()
+        n = X_test.shape[0]
+        k = X_test.shape[1]
+        X_test = (X_test - self.mean_x) / self.std_x
+        X_test = np.hstack([np.ones((n, 1)), X_test])
+        weights = self.get_weights()
+        return 1 / (1 + np.exp(-(X_test @ weights)))
 
     def get_weights(self):
         """
         Get weights from fitted linear model
         :return: weights array
         """
-        pass
+        if not self.flag:
+            raise Exception()
+        return self.w
--- homeworks/homework_07_ml/metrics/metrics.py
+++ homeworks/homework_07_ml/metrics/metrics.py
@@ -5,14 +5,37 @@
 import numpy as np
 
 
-def logloss(y_true, y_pred):
+def check(arr1, arr2):
+    if arr1.shape[0] != arr2.shape[0]:
+        raise ValueError
+    return True
+
+
+def logloss(y_true, y_pred, eps=1e-8):
     """
     logloss
     :param y_true: vector of truth (correct) class values
     :param y_hat: vector of estimated probabilities
     :return: loss
     """
-    pass
+    if check(y_true, y_pred):
+        y_pred = np.clip(y_pred, eps, 1 - eps)
+        return -sum(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred)) / len(y_true)
+
+
+def variables(y_true, y_pred):
+    # сделал втупую, мб можно красивее
+    tp, tn, fp, fn = 0, 0, 0, 0
+    for i in range(y_true.shape[0]):
+        if y_true[i] == 1 and y_pred[i] == 1:
+            tp += 1
+        if y_true[i] == 0 and y_pred[i] == 0:
+            tn += 1
+        if y_true[i] == 0 and y_pred[i] == 1:
+            fp += 1
+        if y_true[i] == 1 and y_pred[i] == 0:
+            fn += 1
+    return tp, tn, fp, fn
 
 
 def accuracy(y_true, y_pred):
@@ -22,7 +45,9 @@ def accuracy(y_true, y_pred):
     :param y_hat: vector of estimated class values
     :return: loss
     """
-    pass
+    if check(y_true, y_pred):
+        tp, tn, fp, fn = variables(y_true, y_pred)
+        return (tp + tn) / (tp + tn + fp + fn)
 
 
 def presicion(y_true, y_pred):
@@ -32,7 +57,9 @@ def presicion(y_true, y_pred):
     :param y_hat: vector of estimated class values
     :return: loss
     """
-    pass
+    if check(y_true, y_pred):
+        tp, tn, fp, fn = variables(y_true, y_pred)
+        return tp / (tp + fp)
 
 
 def recall(y_true, y_pred):
@@ -42,14 +69,27 @@ def recall(y_true, y_pred):
     :param y_hat: vector of estimated class values
     :return: loss
     """
-    pass
+    if check(y_true, y_pred):
+        tp, tn, fp, fn = variables(y_true, y_pred)
+        return tp / (tp + fn)
 
 
-def roc_auc(y_true, y_pred):
+def roc_auc(y_true, y_pred, step=0.001):
     """
     roc_auc
     :param y_true: vector of truth (correct) target values
     :param y_hat: vector of estimated probabilities
     :return: loss
     """
-    pass
+    if check(y_true, y_pred):
+        tpr = []
+        fpr = []
+        for threshold in np.arange(0, 1 + step, step):
+            y_tmp = np.copy(y_pred)
+            y_tmp[y_tmp >= threshold] = 1
+            y_tmp[y_tmp < threshold] = 0
+            tp, tn, fp, fn = variables(y_true, y_tmp)
+            tpr.append(tp / (tp + fn))
+            fpr.append(fp / (fp + tn))
+            # plt.scatter (fp/(fp+tn),tp/(tp+fn))
+    return -np.trapz(np.array(tpr), np.array(fpr))
